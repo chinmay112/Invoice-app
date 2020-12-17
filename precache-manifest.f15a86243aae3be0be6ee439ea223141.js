fd58e03e764761fad0cfb6a2163ca5ee0784dc1c
chinmay112 <meenapandey@Meenas-MacBook-Pro.local>
2020-12-17 18:34:45 +0530

    Updates

---
+++ precache-manifest.f15a86243aae3be0be6ee439ea223141.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957c91f218d26b27a49ec6ae9c432798",
+    "url": "/Invoice-app/index.html"
+  },
+  {
+    "revision": "968e1ff1e080f2d23e76",
+    "url": "/Invoice-app/static/css/main.d7c75489.chunk.css"
+  },
+  {
+    "revision": "f20902bb3120417f9280",
+    "url": "/Invoice-app/static/js/2.25efce19.chunk.js"
+  },
+  {
+    "revision": "dbe755ed4a5a942ed22a",
+    "url": "/Invoice-app/static/js/3.97378ba7.chunk.js"
+  },
+  {
+    "revision": "bb658d4389fcc3697cdd",
+    "url": "/Invoice-app/static/js/4.18683564.chunk.js"
+  },
+  {
+    "revision": "968e1ff1e080f2d23e76",
+    "url": "/Invoice-app/static/js/main.4d979720.chunk.js"
+  },
+  {
+    "revision": "50d968ee9ae983ac3e7a",
+    "url": "/Invoice-app/static/js/runtime~main.bbbb8f3b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d25fb016cc657c1c4b30fa83f71b97c.js"
+  "/Invoice-app/precache-manifest.f15a86243aae3be0be6ee439ea223141.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Invoice-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
